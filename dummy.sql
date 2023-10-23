f250634f2bdd408d5c9ccdb7934537e65edbae5a
mgalihpp <muhammadgalih451@gmail.com>
2023-10-23 10:29:27 +0700

    dummy sql

---
+++ dummy.sql
@@ -0,0 +1,42 @@
+-- COPY THIS DUMMY DATA
+-- DATABASE : music_streaming
+-- TABLE : musics
+
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (1,'static/files\\y2mate.com - I Wanna Be Yours.mp3','Wanna Be Yours','Artic Monkeys','static/img\\wannabeyours.png');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (2,'static/files\\y2mate.com - scott street but its the extended and slowed outro.mp3','Scott Street','Phoebe Bridgers','static/img\\scoott.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (3,'static/files\\y2mate.com - Ruth B  Dandelions Lyrics.mp3','Dandelions','Ruth B.','static/img\\dandelions.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (4,'static/files\\y2mate.com - Night Changes x Shayad slowedreverb Tiktok Version.mp3','Night Changes x Shayad','One Direction','static/img\\One_Direction_-_Night_Changes_Single_Cover.png');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (5,'static/files\\y2mate.com - Post Malone  Circles.mp3','Circles','Post Malone','static/img\\circles.png');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (6,'static/files\\y2mate.com - Miley Cyrus  Angels Like You.mp3','Angels Like You','Miley Cyrus','static/img\\angels-like-you.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (7,'static/files\\y2mate.com - Tiara Andini  Janji Setia Official Music Video.mp3','Janji Setia','Tiara Andini','static/img\\janji-setia.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (8,'static/files\\y2mate.com - Aziz Hedra  Somebodys Pleasure Lyrics Sped Up.mp3','Somebody\'s Pleasure','Aziz Hedra','static/img\\somebody pleasure.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (9,'static/files\\y2mate.com - Harry Styles  As It Was Official Video.mp3','As It Was','Harry Styles','static/img\\as it was.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (10,'static/files\\y2mate.com - Kaash Paige  Love Songs  Official Music Video.mp3','Love Songs','Kaash Paige','static/img\\kaash-paige.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (11,'static/files\\y2mate.com - Rex Orange County  THE SHADE Lyrics.mp3','THE SHADE','Rex Orange County','static/img\\the-shade.png');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (12,'static/files\\y2mate.com - Rex Orange County  AMAZING Official Video.mp3','AMAZING','Rex Orange County','static/img\\amazing.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (13,'static/files\\y2mate.com - Jeremy Zucker  all the kids are depressed  Lyrics.mp3','all the kids are depressed','Jeremy Zucker','static/img\\all-the-kid-depressed.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (14,'static/files\\y2mate.com - Dawin  Dessert Official Music Video ft Silentó.mp3','Dessert','Dawin','static/img\\dessert.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (15,'static/files\\y2mate.com - Keane  Somewhere Only We Know Official Music Video.mp3','Somewhere Only We Know','Keane','static/img\\somwhereonlyweknow.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (16,'static/files\\y2mate.com - Anggi Marito  Tak Segampang Itu Official Lyric Video.mp3','Tak Segampang Itu','Anggi Marito','static/img\\tak-segampang-itu.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (17,'static/files\\y2mate.com - HER  Best Part Lyrics Ft Daniel Caesar.mp3','Best Part','H.E.R','static/img\\best-partjpg.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (18,'static/files\\y2mate.com - Celina Sharma  Harris J  247 Official Video.mp3','24/7','Celina Sharma & Harris J','static/img\\24-7.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (19,'static/files\\y2mate.com - FIFTY FIFTY  Cupid Twin Version Lyrics.mp3','Cupid','Fifty Fifty','static/img\\cupid.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (20,'static/files\\y2mate.com - Old Love  Yuji  Putri Dahlia Official Lyrics Video.mp3','Old Love','Putri Dahlia','static/img\\old-love.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (21,'static/files\\y2mate.com - Heavenly.mp3','Heavenly','Cigarettes After Sex','static/img\\heavenly.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (22,'static/files\\y2mate.com - Ed Sheeran  Perfect.mp3','Perfect','Ed Sheeran','static/img\\Ed_Sheeran_Perfect_Single_cover.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (23,'static/files\\x2mate.com - TheFatRat _ Anjulie - Close To The Sun (128 kbps).mp3','Close The Sun','TheFatRat','static/img\\download.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (24,'static/files\\y2mate.com - KARNA SU SAYANG  ABIL SKA 86 Feat NIKISUKA  Reggae SKA Version.mp3','Karna Su Sayang','ABIL SKA 86 Feat. NIKISUKA','static/img\\karna-su-sayang.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (25,'static/files\\y2mate.com - Farewell.mp3','Farewell','JoRaffi','static/img\\farewell.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (26,'static/files\\y2mate.com - d4vd  Here With Me Official Music Video.mp3','Here With Me','d4vd','static/img\\herewithme.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (27,'static/files\\y2mate.com - NIKI  Take A Chance With Me Official Lyric Video.mp3','Take A Chance With Me','NIKI','static/img\\take-a-chance-with-me.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (28,'static/files\\y2mate.com - Apocalypse  Cigarettes After Sex.mp3','Apocalypse','Cigarettes After Sex','static/img\\apocalypse.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (29,'static/files\\y2mate.com - Melanie Martinez  Play Date Lyrics Slowed Pitched_2.mp3','Play Date','Melanie Martinez','static/img\\playdate.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (30,'static/files\\y2mate.com - Vira Talisa Dharmawan  Walking Back Home.mp3','Walking Back Home','Vira Talisa Dharmawan','static/img\\walking-back-home.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (31,'static/files\\y2mate.com - Shoti  LDR.mp3','LDR','Shoti','static/img\\LDR.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (32,'static/files\\y2mate.com - thuy  girls like me dont cry official music video.mp3','girls like me don’t cry','thuy','static/img\\girls-like-me-dont-cry.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (33,'static/files\\y2mate.com - Akon  Lonely.mp3','Lonely','Akon','static/img\\lonely.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (34,'static/files\\y2mate.com - Glass Animals  Heat Waves.mp3','Heat Waves','Glass Animals','static/img\\Glass_Animals_-_Heat_Waves.png');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (35,'static/files\\y2mate.com - Steve Lacy  Bad Habit Official Video.mp3','Bad Habit','Steve Lacy','static/img\\bad-habbit.jpg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (36,'static/files\\y2mate.com - YKWIM.mp3','YKWIM','Yot Club','static/img\\ykwim.jpeg');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (37,'static/files\\Rizky Febian  Jangan Bawa Perasaan Bercanda Official Lyric Video.mp3','Bercanda','Rizkuy Febian','static/img\\Screenshot 2023-10-22 200522.png');
+INSERT INTO `musics` (`id`,`path`,`name`,`artist`,`image`) VALUES (38,'static/files\\Justin Bieber - Pray.mp3','Pray','Justin Bieber','static/img\\justinbieberpray.jpg');

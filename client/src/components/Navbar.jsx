691c10548f27e306e2e7dcd6d6193e36cd93fc1d
mgalihpp <muhammadgalih451@gmail.com>
2023-10-24 01:08:58 +0700

    add playlist

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,34 +1,66 @@
-import { Home, PlusSquare, Search } from "lucide-react";
+import { Home, Library, Plus, PlusSquare, Search } from "lucide-react";
 import { Link } from "react-router-dom";
+import PlaylistMusic from "./PlaylistMusic";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="space-y-4">
+      <nav className="space-y-1 rounded-md bg-white/5">
         <Link
           title="Home"
-          to='/'
-          className="flex items-center text-base text-zinc-200 gap-3 font-semibold rounded-md p-3.5 hover:bg-white/5"
+          to="/"
+          className="flex items-center text-base text-zinc-200 gap-4 font-bold rounded-md p-3.5 hover:bg-white/5"
         >
-          <Home /> Home
+          <Home fill="white" className="text-black w-7 h-7" /> Home
         </Link>
         <Link
           title="Search?"
-          to='/search'
-          className="flex items-center text-base text-zinc-200 gap-3 font-semibold rounded-md p-3.5 hover:bg-white/5"
+          to="/search"
+          className="flex items-center text-base text-zinc-200 gap-4 font-bold rounded-md p-3.5 hover:bg-white/5"
         >
-          <Search /> Search
+          <Search className="w-7 h-7" /> Search
         </Link>
         <Link
           title="Upload Music?"
-          to='/upload'
-          className="flex items-center text-base text-zinc-200 gap-3 font-semibold rounded-md p-3.5 hover:bg-white/5"
+          to="/upload"
+          className="flex items-center text-base text-zinc-200 gap-4 font-bold rounded-md p-3.5 hover:bg-white/5"
         >
-          <PlusSquare /> Upload
+          <PlusSquare className="w-7 h-7" /> Upload
         </Link>
       </nav>
 
-      <hr className="border border-zinc-700 mt-5" />
+      <div className="p-1 rounded-md bg-white/5 mt-2">
+        <div className="flex items-center justify-between text-zinc-400 gap-2 mb-5 ml-1 px-4 py-1">
+          <div className="flex items-center justify-center gap-2">
+            <Library className="w-7 h-7" />
+            <h1 className="text-lg font-bold">Your Playlist</h1>
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+            className="rounded-full bg-white/5 group p-2"
+            title="Create a Playlist?">
+              <Plus className="w-4 h-4 text-zinc-400 group-hover:text-white transition-all" />
+            </button>
+          </div>
+        </div>
+        <nav
+          className="grid grid-cols-2 p-1 mx-auto overflow-invisible hover:overflow-y-auto"
+          style={{ maxHeight: "390px", overflowX: "hidden" }}
+        >
+          <PlaylistMusic>Favorite Musics</PlaylistMusic>
+          <PlaylistMusic>My Playlist</PlaylistMusic>
+          <PlaylistMusic>Favorite Musics</PlaylistMusic>
+          <PlaylistMusic>My Playlist</PlaylistMusic>
+          <PlaylistMusic>Favorite Musics</PlaylistMusic>
+          <PlaylistMusic>My Playlist</PlaylistMusic>
+          <PlaylistMusic>Favorite Musics</PlaylistMusic>
+          <PlaylistMusic>My Playlist</PlaylistMusic>
+          <PlaylistMusic>Favorite Musics</PlaylistMusic>
+          <PlaylistMusic>My Playlist</PlaylistMusic>
+          <PlaylistMusic>Favorite Musics</PlaylistMusic>
+          <PlaylistMusic>My Playlist</PlaylistMusic>
+        </nav>
+      </div>
     </>
   );
 };
---
+++ client/src/components/PlaylistMusic.jsx
@@ -0,0 +1,28 @@
+import { Play } from "lucide-react";
+import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+
+const PlaylistMusic = ({ children }) => {
+  return (
+    <Link
+      to="/playlist"
+      className="flex items-start justify-center group w-[120px] flex-col text-base text-zinc-200 gap-3 font-semibold rounded p-3 hover:bg-white/10 transition-all"
+    >
+      <div className="relative">
+        <img src="/img/download.jpeg" className="rounded" />
+        <button className="absolute flex items-center justify-center bottom-2 transform translate-y-2 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 right-1 pl-3.5 p-2 rounded-full bg-green-500/80 text-black button-transition hover:scale-110 hover:bg-green-500">
+          <Play fill="black" />
+        </button>
+      </div>
+      <p className="font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis w-24">
+        {children}
+      </p>
+    </Link>
+  );
+};
+
+PlaylistMusic.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default PlaylistMusic;

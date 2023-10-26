a605dc6bfc23dd443cc4769943439a82fb265a8a
mgalihpp <muhammadgalih451@gmail.com>
2023-10-26 19:14:50 +0700

    fix bug when fetching

--- client/src/Context/AudioContext.jsx
+++ client/src/Context/AudioContext.jsx
@@ -1,19 +1,64 @@
-import { createContext, useState } from "react";
-import { PropTypes } from 'prop-types';
+import { createContext, useContext, useState } from "react";
+import { PropTypes } from "prop-types";
+import { useMusicContext } from "./MusicContext";
 
 export const AudioContext = createContext();
+
+export function useAudioContext() {
+  return useContext(AudioContext);
+}
 
 export function AudioProvider({ children }) {
   const [selectedAudio, setSelectedAudio] = useState(null);
   const [isPause, setIsPause] = useState(true);
 
+  // Initialize currentIndex and musicData
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const { musicData } = useMusicContext(); // Use musicData from useMusicContext
+
+  const playNext = () => {
+    if (currentIndex !== -1 && currentIndex < musicData.length - 1) {
+      const nextIndex = currentIndex + 1;
+      const nextMusic = musicData[nextIndex];
+      setSelectedAudio({ ...nextMusic, isPause: false });
+      setCurrentIndex(nextIndex);
+    }
+    if (currentIndex >= musicData.length - 1) {
+      const nextIndex = 0;
+      const resetMusic = musicData[nextIndex];
+      setSelectedAudio({ ...resetMusic, isPause: false });
+      setCurrentIndex(nextIndex);
+    }
+  };
+
+  const playPrevious = () => {
+    if (currentIndex > 0) {
+      const previousIndex = currentIndex - 1;
+      const previousMusic = musicData[previousIndex];
+      setSelectedAudio({ ...previousMusic, isPause: false });
+      setCurrentIndex(previousIndex);
+    }
+  };
+
   return (
-    <AudioContext.Provider value={{ selectedAudio, setSelectedAudio, isPause, setIsPause }}>
+    <AudioContext.Provider
+      value={{
+        selectedAudio,
+        setSelectedAudio,
+        isPause,
+        setIsPause,
+        musicData,
+        playNext,
+        playPrevious,
+        currentIndex,
+        setCurrentIndex,
+      }}
+    >
       {children}
     </AudioContext.Provider>
   );
 }
 
 AudioProvider.propTypes = {
-  children: PropTypes.node.isRequired
-}
+  children: PropTypes.node.isRequired,
+};
--- client/src/Context/MusicContext.jsx
+++ client/src/Context/MusicContext.jsx
@@ -1,6 +1,12 @@
-// MusicContext.js
-import { createContext, useContext, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { PropTypes } from "prop-types";
+import { useUploadContext } from "./UploadContext";
 
 const MusicContext = createContext();
 
@@ -8,16 +14,16 @@ export function useMusicContext() {
   return useContext(MusicContext);
 }
 
-const timestamp = Date.now();
-const url = `http://localhost:5000/music?t=${timestamp}`;
-
+const url = `http://127.0.0.1:5000`;
 export function MusicProvider({ children }) {
   const [musicData, setMusicData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const { isFetching, setIsFetching } = useUploadContext();
+  const [searchResults, setSearchResults] = useState([]);
 
-  async function fetchData() {
+  const fetchData = useCallback(async () => {
     try {
-      const response = await fetch(url, {
+      const response = await fetch(`${url}/musics`, {
         method: "GET",
         headers: {
           "Access-Control-Allow-Headers": "Content-Type",
@@ -26,7 +32,7 @@ export function MusicProvider({ children }) {
       });
       if (response.ok) {
         const data = await response.json();
-        setMusicData(data);
+        setMusicData(data.musics);
         setIsLoading(false);
       } else {
         console.error("Failed to fetch data");
@@ -34,10 +40,55 @@ export function MusicProvider({ children }) {
     } catch (error) {
       console.error("Error: " + error);
     }
+  }, []);
+
+  async function searchMusic(query) {
+    try {
+      const response = await fetch(`${url}/search/music?n=${query}`, {
+        method: "GET",
+        headers: {
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Methods": "GET",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setSearchResults(data.results);
+        setIsLoading(false);
+      } else {
+        console.error("Failed To Search Music");
+      }
+    } catch (error) {
+      console.error("Error: " + error);
+    }
   }
 
+  useEffect(() => {
+    if (musicData.length === 0 && isLoading && !isFetching) {
+      fetchData();
+    }
+  }, [musicData, fetchData, isFetching, isLoading]);
+
+  useEffect(() => {
+    if (isFetching) {
+      setTimeout(() => {
+        fetchData();
+        setIsFetching(false);
+      }, 1000);
+    }
+  }, [isFetching, setIsFetching, fetchData]);
+
   return (
-    <MusicContext.Provider value={{ musicData, isLoading, fetchData }}>
+    <MusicContext.Provider
+      value={{
+        musicData,
+        searchResults,
+        isLoading,
+        setIsLoading,
+        fetchData,
+        searchMusic,
+      }}
+    >
       {children}
     </MusicContext.Provider>
   );

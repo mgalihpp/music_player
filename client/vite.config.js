4556e4707606f75575399b17bea3662706b53f7f
mgalihpp <muhammadgalih451@gmail.com>
2023-10-28 23:44:34 +0700

    add color thief

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["color-thief-react"],
+  },
   server: {
     host: "127.0.0.1",
     port: 5173,

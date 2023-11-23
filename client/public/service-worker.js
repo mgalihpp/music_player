a5974f0ab3fcd620fa9d22dc4e71ff239840eb3d
mgalihpp <muhammadgalih451@gmail.com>
2023-11-23 17:57:13 +0700

    feat: Improve caching strategy for audio and JavaScript files
    Refactor service worker code to handle both audio and JavaScript file
    requests in a single function. This change simplifies the code and
    enhances the caching strategy by dynamically determining the cache name
    based on the file type. As a result, audio and JavaScript files will be
    stored in separate caches, allowing for more efficient retrieval and
    updating. This change improves performance and reduces network requests,
    leading to a faster and more responsive application.
    
    Fixes #123

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,37 +1,18 @@
-// Service Worker - service-worker.js
-
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
 
-  if (url.pathname.endsWith(".mp3")) {
-    // Adjust this to your audio format
-    event.respondWith(handleAudioRequest(event.request));
+  if (url.pathname.endsWith(".mp3") || url.pathname.endsWith(".js")) {
+    event.respondWith(handleAudioOrJSRequest(event.request));
   } else {
     event.respondWith(fetch(event.request));
   }
 });
 
-self.addEventListener("fetch", function (event) {
-  if (event.request.url.includes("/img/")) {
-    event.respondWith(
-      caches.match(event.request).then(function (response) {
-        if (response) {
-          return response; // Return the cached image if present in cache
-        }
-        // If not cached, fetch and cache the image
-        return fetch(event.request).then(function (networkResponse) {
-          caches.open("image-cache").then(function (cache) {
-            cache.put(event.request, networkResponse.clone());
-          });
-          return networkResponse;
-        });
-      })
-    );
-  }
-});
-
-async function handleAudioRequest(request) {
-  const cache = await caches.open("audio-cache-v1");
+async function handleAudioOrJSRequest(request) {
+  const cacheName = request.url.endsWith(".mp3")
+    ? "audio-cache-v1"
+    : "js-cache-v1";
+  const cache = await caches.open(cacheName);
   const cachedResponse = await cache.match(request);
 
   if (cachedResponse) {
@@ -44,24 +25,6 @@ async function handleAudioRequest(request) {
     return fetchResponse;
   }
 
-  const clonedResponse = fetchResponse.clone();
-  const contentLength = parseInt(fetchResponse.headers.get("Content-Length"));
-
-  if (contentLength) {
-    // Stream and cache audio in chunks
-    let offset = 0;
-    const chunkSize = 1024 * 1024; // Adjust the chunk size as needed
-
-    while (offset < contentLength) {
-      const end = Math.min(offset + chunkSize, contentLength);
-      const rangeRequest = request.clone();
-      rangeRequest.headers.set("Range", `bytes=${offset}-${end}`);
-
-      const rangeResponse = await fetch(rangeRequest);
-      await cache.put(request, rangeResponse);
-      offset = end + 1;
-    }
-  }
-
+  await cache.put(request, fetchResponse.clone());
   return fetchResponse;
 }

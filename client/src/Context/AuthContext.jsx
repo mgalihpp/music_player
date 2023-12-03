2457e0b93eb7ba4528d264ff5b5fb3175117cef7
mgalihpp <muhammadgalih451@gmail.com>
2023-12-03 13:59:08 +0700

    feat: Add user info fetching to AuthContext
    This change adds functionality to fetch user information and store it in
    the
    AuthContext. By making a GET request to the server with the user ID, the
    user's
    information is retrieved and stored in the `userInfo` state variable.
    This will
    allow convenient access to user information throughout the application.
    The
    `userInfo` state variable will be updated whenever the `userId` state
    variable
    changes. This change addresses the need for retrieving and storing user
    information in the authentication context.

--- client/src/Context/AuthContext.jsx
+++ client/src/Context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { PropTypes } from "prop-types";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { api } from "./../utils";
 
 const AuthContext = createContext();
 
@@ -10,9 +11,33 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(false);
   const [userId, setUserId] = useState(localStorage.getItem("user_id") || null);
+  const [userInfo, setUserInfo] = useState({});
+
+  console.log(userInfo);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`${api}user?id=${userId}`, {
+          method: "GET",
+        });
+        if (res.ok) {
+          const data = await res.json();
+          setUserInfo(data);
+        } else {
+          throw new Error("Failed to fetch user info");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (userId !== null) getUser();
+  }, [userId]);
 
   return (
-    <AuthContext.Provider value={{ user, setUser, userId, setUserId }}>
+    <AuthContext.Provider
+      value={{ user, setUser, userId, setUserId, userInfo }}
+    >
       {children}
     </AuthContext.Provider>
   );

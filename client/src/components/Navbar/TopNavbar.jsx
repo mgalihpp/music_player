fc40d69dfc6e1c87743a80cfa86fd63492f4debf
mgalihpp <muhammadgalih451@gmail.com>
2023-12-05 23:32:57 +0700

    feat: Add react-sticky-nav package for sticky navigation
    This change adds the react-sticky-nav package to the client's
    dependencies, allowing for the implementation of sticky navigation in
    the TopNavbar component. Sticky navigation improves the UX by keeping
    the navigation menu visible when scrolling. The package is configured
    with peerDependencies for React and React DOM versions 16.8 or higher.
    This change enhances the navigation experience for users.

--- client/src/components/Navbar/TopNavbar.jsx
+++ client/src/components/Navbar/TopNavbar.jsx
@@ -3,40 +3,54 @@ import { useNavigate } from "react-router-dom";
 import DropdownMenu from "./DropDownMenu";
 import { PropTypes } from "prop-types";
 import MobileNavbar from "./MobileNavbar";
+import Navigation from "react-sticky-nav";
+import { useTheme } from "../../Context/ThemeContext";
 
-const TopNavbar = ({ children, className }) => {
+const TopNavbar = ({ children, className, position }) => {
   const navigate = useNavigate();
+  const { currentTheme } = useTheme();
 
   const handleNavigation = (step) => {
     navigate(step);
   };
 
   return (
-    <div className={`flex items-center justify-between gap-4 ${className}`}>
-      <div className="flex items-center gap-4">
-        <MobileNavbar/>
-        <button
-          onClick={() => handleNavigation(-1)}
-          title="Go Back"
-          className="p-1 rounded-full bg-white/5 hover:bg-white/10"
-          aria-label="left"
-        >
-          <ChevronLeft />
-        </button>
-        <button
-          onClick={() => handleNavigation(1)}
-          title="Go Next"
-          className="p-1 rounded-full bg-white/5 hover:bg-white/10"
-          aria-label="right"
-        >
-          <ChevronRight />
-        </button>
-        {children}
+    <Navigation className="z-[1]">
+      <div
+        className={`flex items-center justify-between gap-4 z-[1] ${
+          position >= 20
+            ? `${currentTheme === "dark" ? "bg-zinc-900" : ""} `
+            : "transparent"
+        } ${className}`}
+        style={{
+          transition: "background-color 0.3s ease-in-out", // Smooth transition
+        }}
+      >
+        <div className="flex items-center gap-4">
+          <MobileNavbar />
+          <button
+            onClick={() => handleNavigation(-1)}
+            title="Go Back"
+            className="p-1 rounded-full bg-white/5 hover:bg-white/10"
+            aria-label="left"
+          >
+            <ChevronLeft />
+          </button>
+          <button
+            onClick={() => handleNavigation(1)}
+            title="Go Next"
+            className="p-1 rounded-full bg-white/5 hover:bg-white/10"
+            aria-label="right"
+          >
+            <ChevronRight />
+          </button>
+          {children}
+        </div>
+        <div className="flex items-center gap-2">
+          <DropdownMenu />
+        </div>
       </div>
-      <div className="flex items-center gap-2">
-        <DropdownMenu />
-      </div>
-    </div>
+    </Navigation>
   );
 };
 
@@ -45,4 +59,5 @@ export default TopNavbar;
 TopNavbar.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  position: PropTypes.string,
 };

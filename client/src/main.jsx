cdd4156209b2ab8e23145fecdd387fa1c9cd5376
mgalihpp <muhammadgalih451@gmail.com>
2023-12-07 07:58:44 +0700

    remove service worker

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,29 +12,29 @@ import { Theme } from "@radix-ui/themes";
 import { AuthProvider } from "./Context/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        // Force the service worker to activate immediately after registration
-        registration.addEventListener("updatefound", () => {
-          const newWorker = registration.installing;
-          if (newWorker) {
-            newWorker.addEventListener("statechange", () => {
-              if (newWorker.state === "installed") {
-                // Ensure the service worker is activated
-                newWorker.postMessage({ action: "SKIP_WAITING" });
-              }
-            });
-          }
-        });
-      })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
-      });
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/service-worker.js")
+//       .then((registration) => {
+//         // Force the service worker to activate immediately after registration
+//         registration.addEventListener("updatefound", () => {
+//           const newWorker = registration.installing;
+//           if (newWorker) {
+//             newWorker.addEventListener("statechange", () => {
+//               if (newWorker.state === "installed") {
+//                 // Ensure the service worker is activated
+//                 newWorker.postMessage({ action: "SKIP_WAITING" });
+//               }
+//             });
+//           }
+//         });
+//       })
+//       .catch((error) => {
+//         console.error("Service Worker registration failed:", error);
+//       });
+//   });
+// }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

820229a927f2bf86eacc72b0c3c73afe19f37a0f
mgalihpp <muhammadgalih451@gmail.com>
2023-12-07 11:19:30 +0700

    Refactor footer component to include links and improve readability
    This refactor updates the Footer component in order to include links to
    the About page and GitHub repository. The changes also improve the
    overall readability and organization of the code. The goal of these
    modifications is to enhance user navigation and provide easy access to
    additional information and resources. No known issues are expected from
    this update.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,63 +1,29 @@
-import {
-  Laptop2,
-  LayoutList,
-  Maximize2,
-  Mic2,
-  Play,
-  Repeat,
-  Shuffle,
-  SkipBack,
-  SkipForward,
-  Volume,
-} from "lucide-react";
+import { Link } from "react-router-dom";
 
-export const Footer = () => {
+const Footer = () => {
   return (
-    <>
-      <div className="flex items-center gap-3">
-        <img src="/img/download.jpeg" alt="cover" width={56} height={56} />
-        <div className="flex flex-col">
-          <a href="">
-            <strong className="font-normal hover:underline transition-all">
-              Close The Sun
-            </strong>
-          </a>
-          <span className="text-xs text-zinc-400">TheFatRat</span>
-        </div>
+    <footer className="flex flex-row justify-between px-4 sm:px-8 pt-24 pb-4">
+      <div className="flex flex-col items-start justify-center gap-2">
+        <h1 className="text-lg font-bold">This Page</h1>
+        <Link
+          to="/about"
+          className="text-zinc-400 text-sm font-semibold hover:underline hover:text-zinc-100 transition-all"
+        >
+          About
+        </Link>
+        <Link
+          target="_blank"
+          to="https://github.com/mgalihpp/music_player"
+          className="text-zinc-400 text-sm font-semibold hover:underline hover:text-zinc-100 transition-all"
+        >
+          Github
+        </Link>
       </div>
-      <div className="flex flex-col items-center gap-2">
-        <div className="flex items-center gap-6">
-          <Shuffle size={20} className="text-zinc-200" />
-          <SkipBack size={20} className="text-zinc-200" />
-          <button
-            aria-label="play"
-            className="flex items-center justify-center pl-2.5 p-2 rounded-full bg-white text-black"
-          >
-            <Play className="fill-black" />
-          </button>
-          <SkipForward size={20} className="text-zinc-200" />
-          <Repeat size={20} className="text-zinc-200" />
-        </div>
-        <div className="flex items-center gap-2">
-          <span className="text-xs text-zinc-500">0:31</span>
-          <div className="h-1 rounded-full w-96 bg-zinc-600">
-            <div className="bg-zinc-200 w-48 h-1 rounded-full"></div>
-          </div>
-          <span className="text-xs text-zinc-500">2:31</span>
-        </div>
+      <div className="flex flex-col items-end justify-center">
+        <h1>&copy; mgpp</h1>
       </div>
-      <div className="flex items-center gap-4">
-        <Mic2 size={20} />
-        <LayoutList size={20} />
-        <Laptop2 size={20} />
-        <div className="flex items-center gap-2">
-          <Volume size={20} />
-          <div className="h-1 rounded-full w-24 bg-zinc-600">
-            <div className="bg-zinc-200 w-16 h-1 rounded-full"></div>
-          </div>
-        </div>
-        <Maximize2 size={20} />
-      </div>
-    </>
+    </footer>
   );
 };
+
+export default Footer;

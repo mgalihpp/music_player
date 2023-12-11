a051e8d26fbd0cf71dc2ead735553dc9089d1862
mgalihpp <muhammadgalih451@gmail.com>
2023-12-11 22:07:12 +0700

    Add responsive behavior to MusicCard component
    This change introduces responsive behavior to the MusicCard component by
    utilizing the react-responsive package. Two media queries, one for
    mobile and one for desktop, are used to conditionally render different
    elements. The mobile view now displays a clickable card with the music
    name and artist, as well as a play button. The desktop view displays a
    larger card with an image, play button, and music name. This enhances
    the user experience by providing a more intuitive and visually appealing
    layout on different devices.

--- client/src/components/MusicCard.jsx
+++ client/src/components/MusicCard.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { PropTypes } from "prop-types";
 import { useMusicContext } from "../Context/MusicContext";
 import { host } from "../utils";
+import { useMediaQuery } from "react-responsive";
 
 const MusicCard = (props) => {
   const {
@@ -26,6 +27,8 @@ const MusicCard = (props) => {
   } = useAudioContext();
   const { musicData } = useMusicContext();
   const navigate = useNavigate();
+  const isMobile = useMediaQuery({ minWidth: 280 });
+  const isDesktop = useMediaQuery({ minWidth: 640 });
 
   useEffect(() => {
     if (selectedAudio?.musicName === musicName) {
@@ -54,119 +57,122 @@ const MusicCard = (props) => {
     <>
       {/* Mobile */}
 
-      <div
-        onClick={() => {
-          if (playlist) {
-            navigate("/user/playlist/" + playlistId);
-          } else {
-            return;
-          }
-        }}
-        className="flex hhh min-w-[270px] h-full sm:hidden flex-col justify-center items-center text-zinc-200 gap-2 font-semibold rounded-md bg-white/5 hover:bg-white/10 transition-all"
-      >
-        <table className="w-full h-full">
-          <tbody>
-            <tr className="flex items-center">
-              <td className="flex flex-row items-center justify-center gap-2">
-                <div className="relative flex items-center justify-center">
-                  <img
-                    src={
-                      musicImage
-                        ? `${host + "img/" + musicImage}`
-                        : playlistImage
-                        ? `${host + "playlist/img/" + playlistImage}`
-                        : "/img/download.jpeg"
-                    }
-                    alt="cover"
-                    className="rounded-lg object-cover w-[50px] h-[50px]"
-                  />
-                </div>
-                <div className="flex flex-col items-start">
-                  <Link
-                    to={
-                      playlist
-                        ? "/user/playlist/" + playlistId
-                        : "/music/" + musicName
-                    }
-                  >
-                    <span className="text-xs max-w-[150px] whitespace-nowrap overflow-hidden overflow-ellipsis">
-                      {musicName}
+      {isMobile && (
+        <div
+          onClick={() => {
+            if (playlist) {
+              navigate("/user/playlist/" + playlistId);
+            } else {
+              return;
+            }
+          }}
+          className="flex hhh min-w-[270px] h-full sm:hidden flex-col justify-center items-center text-zinc-200 gap-2 font-semibold rounded-md bg-white/5 hover:bg-white/10 transition-all"
+        >
+          <table className="w-full h-full">
+            <tbody>
+              <tr className="flex items-center">
+                <td className="flex flex-row items-center justify-center gap-2">
+                  <div className="relative flex items-center justify-center">
+                    <img
+                      src={
+                        musicImage
+                          ? `${host + "img/" + musicImage}`
+                          : playlistImage
+                          ? `${host + "playlist/img/" + playlistImage}`
+                          : "/img/download.jpeg"
+                      }
+                      alt="cover"
+                      className="rounded-lg object-cover w-[50px] h-[50px]"
+                    />
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <Link
+                      to={
+                        playlist
+                          ? "/user/playlist/" + playlistId
+                          : "/music/" + musicName
+                      }
+                    >
+                      <span className="text-xs max-w-[150px] whitespace-nowrap overflow-hidden overflow-ellipsis">
+                        {musicName}
+                      </span>
+                    </Link>
+                    <span className="text-[10px] text-zinc-400 max-w-[150px] whitespace-nowrap overflow-hidden overflow-ellipsis">
+                      {musicArtist}
                     </span>
-                  </Link>
-                  <span className="text-[10px] text-zinc-400 max-w-[150px] whitespace-nowrap overflow-hidden overflow-ellipsis">
-                    {musicArtist}
-                  </span>
-                </div>
-              </td>
-              <td className="ml-auto w-6 flex items-center justify-center mr-2">
-                <div
-                  className={`playing-mobile ${
-                    isCurrentSelected && !isPause ? "visible" : "invisible"
-                  }`}
-                >
-                  <span className="playing__bar playing__bar1"></span>
-                  <span className="playing__bar playing__bar2"></span>
-                  <span className="playing__bar playing__bar3"></span>
-                </div>
-                <button
-                  onClick={handlePlayClick}
-                  className={`items-center justify-center mx-auto ${
-                    isCurrentSelected && isPause
-                      ? "flex"
-                      : isCurrentSelected && !isPause
-                      ? "hidden"
-                      : "flex"
-                  }
+                  </div>
+                </td>
+                <td className="ml-auto w-6 flex items-center justify-center mr-2">
+                  <div
+                    className={`playing-mobile ${
+                      isCurrentSelected && !isPause ? "visible" : "invisible"
+                    }`}
+                  >
+                    <span className="playing__bar playing__bar1"></span>
+                    <span className="playing__bar playing__bar2"></span>
+                    <span className="playing__bar playing__bar3"></span>
+                  </div>
+                  <button
+                    onClick={handlePlayClick}
+                    className={`items-center justify-center mx-auto ${
+                      isCurrentSelected && isPause
+                        ? "flex"
+                        : isCurrentSelected && !isPause
+                        ? "hidden"
+                        : "flex"
+                    }
           `}
-                >
-                  <Play fill="white" color="white" size={15} />
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+                  >
+                    <Play fill="white" color="white" size={15} />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
 
       {/* Dekstop */}
-      <div
-        onClick={() => {
-          if (playlist) {
-            navigate("/user/playlist/" + playlistId);
-          } else {
-            return;
-          }
-        }}
-        className={` cursor-pointer
+      {isDesktop && (
+        <div
+          onClick={() => {
+            if (playlist) {
+              navigate("/user/playlist/" + playlistId);
+            } else {
+              return;
+            }
+          }}
+          className={` cursor-pointer
           hidden sm:flex items-start justify-center p-4 ${
             playlist ? "" : "group"
           } responsive-card flex-col text-base text-zinc-200 gap-3 font-semibold rounded-md bg-white/5 hover:bg-white/10 transition-all`}
-      >
-        <div className="relative flex items-center justify-center">
-          <img
-            src={
-              musicImage
-                ? `${host + "img/" + musicImage}`
-                : playlistImage
-                ? `${host + "playlist/img/" + playlistImage}`
-                : "/img/download.jpeg"
-            }
-            alt="cover"
-            className="rounded-lg relative object-cover responsive-card-img"
-          />
-          <div
-            className={`playing-ui ${
-              isCurrentSelected && !isPause ? "visible" : "invisible"
-            }`}
-          >
-            <span className="playing__bar playing__bar1"></span>
-            <span className="playing__bar playing__bar2"></span>
-            <span className="playing__bar playing__bar3"></span>
-          </div>
-          <button
-            title="Play"
-            aria-label="Play"
-            onClick={handlePlayClick}
-            className={`absolute flex items-center justify-center bottom-2 transform translate-y-2 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 right-2 p-4 rounded-full bg-green-500/80 text-black button-transition hover:scale-110 hover:bg-green-500 hover:shadow-md 
+        >
+          <div className="relative flex items-center justify-center">
+            <img
+              src={
+                musicImage
+                  ? `${host + "img/" + musicImage}`
+                  : playlistImage
+                  ? `${host + "playlist/img/" + playlistImage}`
+                  : "/img/download.jpeg"
+              }
+              alt="cover"
+              className="rounded-lg relative object-cover responsive-card-img"
+            />
+            <div
+              className={`playing-ui ${
+                isCurrentSelected && !isPause ? "visible" : "invisible"
+              }`}
+            >
+              <span className="playing__bar playing__bar1"></span>
+              <span className="playing__bar playing__bar2"></span>
+              <span className="playing__bar playing__bar3"></span>
+            </div>
+            <button
+              title="Play"
+              aria-label="Play"
+              onClick={handlePlayClick}
+              className={`absolute flex items-center justify-center bottom-2 transform translate-y-2 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 right-2 p-4 rounded-full bg-green-500/80 text-black button-transition hover:scale-110 hover:bg-green-500 hover:shadow-md 
           ${
             isCurrentSelected && isPause
               ? "flex group-hover:flex"
@@ -175,29 +181,32 @@ const MusicCard = (props) => {
               : "flex group-hover:flex"
           }
           `}
-          >
-            <Play fill="black" className="ml-1" />
-          </button>
-        </div>
-        <div className="flex flex-col items-start sm:p-0">
-          <Link
-            to={
-              playlist ? "/user/playlist/" + playlistId : "/music/" + musicName
-            }
-          >
-            <p
-              className={`font-semibold px-2 sm:px-0 ${
-                playlist ? "" : "hover:underline"
-              } text-[10px] sm:text-sm whitespace-nowrap overflow-hidden overflow-ellipsis w-full max-w-[100px]`}
             >
-              <strong title={musicName}>{musicName}</strong>
+              <Play fill="black" className="ml-1" />
+            </button>
+          </div>
+          <div className="flex flex-col items-start sm:p-0">
+            <Link
+              to={
+                playlist
+                  ? "/user/playlist/" + playlistId
+                  : "/music/" + musicName
+              }
+            >
+              <p
+                className={`font-semibold px-2 sm:px-0 ${
+                  playlist ? "" : "hover:underline"
+                } text-[10px] sm:text-sm whitespace-nowrap overflow-hidden overflow-ellipsis w-full max-w-[100px]`}
+              >
+                <strong title={musicName}>{musicName}</strong>
+              </p>
+            </Link>
+            <p className="font-normal px-2 sm:px-0 text-xs text-zinc-400 whitespace-nowrap overflow-hidden overflow-ellipsis w-full max-w-[100px]">
+              {musicArtist}
             </p>
-          </Link>
-          <p className="font-normal px-2 sm:px-0 text-xs text-zinc-400 whitespace-nowrap overflow-hidden overflow-ellipsis w-full max-w-[100px]">
-            {musicArtist}
-          </p>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
